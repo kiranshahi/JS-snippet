c1211cda42ae78f2f2818463858d7259e5411c22
Kiran Shahi <kiran.shahi.np@gmail.com>
2017-12-28 11:51:46 +0545

    String to slug updated.

--- stringToSlug.js
+++ stringToSlug.js
@@ -2,6 +2,9 @@ stringToSlug: function (title) {
 	return title.toLowerCase().trim()
 	.replace(/\s+/g, '-')           // Replace spaces with -
 	.replace(/&/g, '-and-')         // Replace & with 'and'
+	// comment folowing line if you want to replace all the unicharacter
+	.replace(/([~!@#$%^&*()_+=`{}\[\]\|\\:;'<>,.\/? ])+/g, '-') // Replace '~!@#$%^&*()_+=`{}\[\]\|\\:;'<>,.\/?' with - suitable for unicode character like nepali 
+	// comment if you want to support uincharacter 
 	.replace(/[^\w\-]+/g, '')       // Remove all non-word chars
 	.replace(/\-\-+/g, '-')         // Replace multiple - with single -
 }
